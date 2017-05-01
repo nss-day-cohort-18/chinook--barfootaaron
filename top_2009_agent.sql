e8a122b790c79195f851cd8343664ed6a4d08dc0
“Aaron <barfootaaron@yahoo.com>
2017-04-30 21:43:35 -0500

    exercises 1-27 complete

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,12 +1,11 @@
 -- Which sales agent made the most in sales in 2009?
 
-
-SELECT e.FirstName 'First Name', e.LastName 'Last Name', e.Title, SUM(i.Total) 'Total Sales'
-FROM Employee e
-JOIN Customer c ON c.SupportRepId = e.EmployeeId
-JOIN Invoice i ON i.CustomerId = c.CustomerId
-WHERE e.Title is 'Sales Support Agent'
-AND InvoiceDate BETWEEN date('2009-01-01') AND date('2009-12-31')
-GROUP BY e.EmployeeId
-
-
+SELECT emp.firstname || " " || emp.lastname  AS sales_rep, "$" || printf("%.2f", SUM(inv.[TOTAL])) AS sales_total
+FROM Employee emp 
+JOIN Customer cus on emp.employeeid = cus.supportrepid
+JOIN Invoice inv on cus.customerid = inv.customerid
+WHERE emp.title = 'Sales Support Agent' 
+AND substr(inv.invoicedate, 0, 5) = '2009'
+GROUP BY emp.employeeid
+ORDER BY SUM(inv.[TOTAL]) desc
+LIMIT 1;
